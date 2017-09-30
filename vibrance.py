81888a20b4dd8b908792fbdc49c251e854bd6016
PukaPy <pukalordu@yahoo.com>
2017-09-30 20:09:09 +0300

    Update vibrance.py

--- vibrance.py
+++ vibrance.py
@@ -3,7 +3,7 @@ import subprocess
 
 VAL = 1023
 ON = f"nvidia-settings -a \"DigitalVibrance={VAL}\" > /dev/null"
-OFF = f"nvidia-settings -a \"DigitalVibrance=0\" > /dev/null"
+OFF = "nvidia-settings -a \"DigitalVibrance=0\" > /dev/null"
 if sys.argv[1] == "on":
     subprocess.check_output(['bash', '-c', ON])
 elif sys.argv[1] == "off":
