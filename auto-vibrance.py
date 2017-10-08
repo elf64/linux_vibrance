085037e6b7921f421bc67780d7cab8bec679dac5
PukaPy <pukalordu@yahoo.com>
2017-10-08 22:20:39 +0300

    Update auto-vibrance.py

--- auto-vibrance.py
+++ auto-vibrance.py
@@ -11,17 +11,20 @@ def set_vibrance(value=300):
             )
     print(f"nvidia-settings vibrance value was set to {value}")
 
+def get_vibrance_value():
+    x = str(subprocess.check_output(
+    ['bash', '-c', 'nvidia-settings -q DigitalVibrance']
+    )).split(" ")
+    return int(x[5].replace(".\\n", ""))
+
 csgo="csgo_linux64"
 csgo_run=False
-off=True
 while 1:
     x=[p.name() for p in psutil.process_iter()]
     if csgo in x and csgo_run == False:
         csgo_run=True
-        off=True
         set_vibrance(1023)
-    if csgo not in x and off == True:
-            csgo_run=False
-            off=False
-            set_vibrance()
+    elif csgo not in x and get_vibrance_value() != 300:
+        csgo_run=False
+        set_vibrance()
     time.sleep(1)
