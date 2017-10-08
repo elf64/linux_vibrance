de3a2c3a786c7d46716bc1e92d0ce9baee26520f
PukaPy <pukalordu@yahoo.com>
2017-10-08 17:10:19 +0300

    Update auto-vibrance.py

--- auto-vibrance.py
+++ auto-vibrance.py
@@ -2,25 +2,26 @@ import sys
 import subprocess
 import psutil
 import time
-# This script turns on/off digital vibrance in nvidia-settings
-#   when csgo starts/closes
-VAL=1023
+
+def set_vibrance(value=300):
+    line=f"nvidia-settings -a \"DigitalVibrance={value}\"\
+            > /dev/null"
+    subprocess.check_output(
+            ['bash', '-c', line]
+            )
+    print(f"nvidia-settings vibrance value was set to {value}")
+
 csgo="csgo_linux64"
-ON=f"nvidia-settings -a \"DigitalVibrance={VAL}\" > /dev/null"
-OFF=f"nvidia-settings -a \"DigitalVibrance=0\" > /dev/null"
 csgo_run=False
 off=True
 while 1:
     x=[p.name() for p in psutil.process_iter()]
-    if csgo in x and csgo_run==False:
-        print("set 1023")
+    if csgo in x and csgo_run == False:
         csgo_run=True
         off=True
-        subprocess.check_output(['bash', '-c', ON])
-    if csgo not in x:
-        if off==True:
-            print("Set variables to False and 0 vibrance")
+        set_vibrance(1023)
+    if csgo not in x and off == True:
             csgo_run=False
             off=False
-            subprocess.check_output(['bash', '-c', OFF])
+            set_vibrance()
     time.sleep(1)
