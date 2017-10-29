f78ea221f2001576472e441e7683c692c1cad5d5
PukaPy <pukalordu@yahoo.com>
2017-10-29 16:47:51 +0200

    Update vibrance.py

--- vibrance.py
+++ vibrance.py
@@ -1,7 +1,8 @@
 import sys
 import subprocess
 
-x=[i for i in sys.argv]
+x = [i for i in sys.argv]
+
 def set_vibrance(value=300):
     line=f"nvidia-settings -a \"DigitalVibrance={value}\" \
         > /dev/null"
@@ -10,15 +11,8 @@ def set_vibrance(value=300):
             )
     print(f"nvidia-settings vibrance value was set to {value}.")
 
-if len(x) == 2:
-    try:
-        set_vibrance(int(x[1]))
-    except ValueError:
-        if x[1] == "on":
-            set_vibrance(1023)
-        else:
-            set_vibrance()
-else:
-    print("No arguments or too many arguments.\
-            \nSetting default vibrance value!")
-    set_vibrance()
+if __name__ == "__main__":
+    try: set_vibrance(int(x[1]))
+    except (ValueError, IndexError) as e:
+        set_vibrance()
+
